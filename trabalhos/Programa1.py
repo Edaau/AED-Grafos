d357056f11c15b3a04c621bb4671ad30f62e4ff8
Eduardo <duedugaranito@gmail.com>
2023-04-07 12:51:26 -0300

    Funcionalidade a completa

--- trabalhos/Programa1.py
+++ trabalhos/Programa1.py
@@ -7,17 +7,23 @@ def Gera_Matriz(M,n,t):
 
 def Funcionalidade_a(M, n, G, h):
     cont = 0
-    for i in range(0,n):
-        for j in range(0,n):
+    for i in range(0,int(n)):
+        for j in range(0,int(n)):
             if i == j:
                 M[i][j] = 0
+            elif M[j][i] == 0:
+                next
+            elif M[i][j] == 1:
+                next
             else:
-                resposta = input("Existe aresta entre os vertices %d e %d ? responda com S/N" % (i , j))
+                resposta = input("Existe aresta entre os vertices %d e %d ? responda com S/N " % (i , j))
                 if resposta == "S" or resposta == "s":
                     M[i][j] = 1
+                    M[j][i] = 1
                     cont += 1
-                elif resposta == "N" or respota == "n":
+                elif resposta == "N" or resposta == "n":
                     M[i][j] = 0
+                    M[j][i] = 0
                 else:
                     print("Insira uma resposta valida")
                     exit(1)
@@ -25,11 +31,16 @@ def Funcionalidade_a(M, n, G, h):
     G[h][0] = name
     G[h][1] = M
     G[h][2] = cont
+    for i in range(0,int(n)):
+        for j in range(0,int(n)):
+            print(M[i][j], end= ' ')
+        print()
+    print("O valor de |E|: %d" % cont)
 
 
 G = []
 grafos = input("Quantos Grafos deseja inserir? ")
-if(grafos.isNumeric()):
+if(grafos.isnumeric()):
     Gera_Matriz(G, int(grafos), 3)
 else:
     print("Insira um numero valido.")
@@ -37,9 +48,9 @@ else:
 for i in range(0, int(grafos)):
     M = []
     n = input("Quantos vertices existem nesse grafo? ")
-    if(n.isNumeric()):
-        Gera_Matriz(M,n,n)
+    if(n.isnumeric()):
+        Gera_Matriz(M,int(n),int(n))
     else:
         print("Insira um numero valido.")
         exit(1)
-    Funcionalidade_a(M, n, G, h)
+    Funcionalidade_a(M, n, G, i)

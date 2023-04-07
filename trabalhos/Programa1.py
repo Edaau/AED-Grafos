5ea41abf43477202a65eb0d2fa3bb6a35d457f1d
Eduardo <duedugaranito@gmail.com>
2023-04-07 11:19:38 -0300

    Funcionalidade a em desenvolvimento

--- trabalhos/Programa1.py
+++ trabalhos/Programa1.py
@@ -4,5 +4,23 @@ def Gera_Matriz(m,n):
         m[i] = []
         for j in range(0,n):
             m[i].append([])
-def Funcionalidade_a():
-    n
+def Funcionalidade_a(m, n):
+    cont = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            if i == j:
+                m[i][j] = 0
+            else:
+                resposta = input("Existe aresta entre os vertices %d e %d ? responda com S/N" % (i , j))
+                if resposta == "S" or resposta == "s":
+                    m[i][j] = 1
+                else:
+                    m[i][j] = 0
+                    
+
+
+
+m = []
+n= 5
+Gera_Matriz(m, n)
+Funcionalidade_a(m, n)

63ed6b1896c78003fe20665d454beae3a1f3677a
Eduardo <duedugaranito@gmail.com>
2023-04-07 11:37:17 -0300

    Funcionalidade adicionando as arestas

--- trabalhos/Programa1.py
+++ trabalhos/Programa1.py
@@ -1,26 +1,34 @@
-def Gera_Matriz(m,n):
+def Gera_Matriz(M,n):
     for i in range(0,n):
-        m.append(0)
-        m[i] = []
+        M.append(0)
+        M[i] = []
         for j in range(0,n):
-            m[i].append([])
-def Funcionalidade_a(m, n):
+            M[i].append([])
+def Funcionalidade_a(M, n, G):
     cont = 0
     for i in range(0,n):
         for j in range(0,n):
             if i == j:
-                m[i][j] = 0
+                M[i][j] = 0
             else:
                 resposta = input("Existe aresta entre os vertices %d e %d ? responda com S/N" % (i , j))
                 if resposta == "S" or resposta == "s":
-                    m[i][j] = 1
+                    M[i][j] = 1
+                    cont += 1
+                else if resposta == "N" or respota == "n":
+                    M[i][j] = 0
                 else:
-                    m[i][j] = 0
+                    print("Insira uma resposta valida")
+                    exit(1)
                     
 
 
 
-m = []
+
+
+M = []
 n= 5
 Gera_Matriz(m, n)
-Funcionalidade_a(m, n)
+G = []
+grafos = int(input)
+Funcionalidade_a(m, n, G)

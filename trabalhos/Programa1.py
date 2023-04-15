dbf235089e8c0214739f04f244f30f210bbefd81
Eduardo <duedugaranito@gmail.com>
2023-04-15 14:33:48 -0300

    Funcionalidade c quase completa

--- trabalhos/Programa1.py
+++ trabalhos/Programa1.py
@@ -53,7 +53,28 @@ def Gera_Matriz_Bipartido(M,n,m,G,h):
         print()
     print("O valor de |E|: %d" % cont)
 
-
+def Gera_Matriz_Estrela(M,n,G,h):
+    cont = 0
+    for i in range(0,int(n)):
+        for j in range(0,int(n)):
+            if M[j][i] == 0:
+                next
+            else:
+                M[i][j] = 0
+                M[j][i] = 0
+    for i in range(1, int(n)):
+        M[0][i] = 1
+        M[i][0] = 1
+        cont += 1
+    name = "estrela_%d" % (int(n))
+    G[h][0] = name
+    G[h][1] = M
+    G[h][2] = cont
+    for i in range(0,int(n)):
+        for j in range(0,int(n)):
+            print(M[i][j], end= ' ')
+        print()
+    print("O valor de |E|: %d" % cont)
 
 def Funcionalidade_a(M, n, G, h):
     cont = 0
@@ -98,9 +119,6 @@ def Funcionalidade_b(G, name, grafos):
             exit(0)
     print("Grafo nao encontrado.")
 
- 
-
-
 G = []
 grafos = input("Quantos Grafos deseja inserir? ")
 if(grafos.isnumeric()):
@@ -108,15 +126,60 @@ if(grafos.isnumeric()):
 else:
     print("Insira um numero valido.")
     exit(1)
-for i in range(0, int(grafos)):
-    M = []
-    n = input("Quantos vertices existem nesse grafo? ")
-    if(n.isnumeric()):
-        Gera_Matriz(M,int(n),int(n))
-    else:
-        print("Insira um numero valido.")
-        exit(1)
-    Funcionalidade_a(M, n, G, i)
+especial = input("Os grafos serao especiais ou nao? Resposta com S/N ")
+if especial == "S" or especial == "s":
+    for i in range(0, int(grafos)):
+        classe = input("De qual classe sera o grafo: ")
+        if classe.upper() == "COMPLETO":
+            M = []
+            n = input("Quantos vertices existem nesse grafo? ")
+            if(n.isnumeric()):
+             Gera_Matriz(M,int(n),int(n))
+            else:
+                print("Insira um numero valido.")
+                exit(1)
+            Gera_Matriz_Completo(M, int(n), G, i)
+        elif classe.upper() == "BIPARTIDO COMPLETO" or classe.upper() == "BIPARTIDOCOMPLETO":
+            M = []
+            n1 = input("Insira o valor de n1: ")
+            if(n1.isnumeric()):
+                n2 = input("Insira o valor de n2: ")
+                if(n2.isnumeric()):
+                    n3 = int(n1)+int(n2)
+                    Gera_Matriz(M,n3,n3)
+                else:
+                    print("Insira um numero valido.")
+                    exit(1)
+            else:
+                print("Insira um numero valido1.")
+                exit(1)
+            Gera_Matriz_Bipartido(M, n1, n2, G, i)
+        elif classe.upper() == "ESTRELA":
+            M = []
+            n = input("Quantos vertices existem nesse grafo? ")
+            if(n.isnumeric()):
+             Gera_Matriz(M,int(n),int(n))
+            else:
+                print("Insira um numero valido.")
+                exit(1)
+            Gera_Matriz_Estrela(M, n, G, i)
+        else:
+            print("Insira uma classe valida na proxima execucao")
+            exit(1)
+
+elif pergunta == "N" or pergunta == "n":
+    for i in range(0, int(grafos)):
+        M = []
+        n = input("Quantos vertices existem nesse grafo? ")
+        if(n.isnumeric()):
+            Gera_Matriz(M,int(n),int(n))
+        else:
+            print("Insira um numero valido.")
+            exit(1)
+        Funcionalidade_a(M, n, G, i)
+else:
+    print("Entre com uma resposta valida na proxima execucao.")
+
 pergunta = input("Deseja encontrar algum grafo?: Responda com S/N ")
 if pergunta == "S" or pergunta == "s":
     nome = input("Insira o nome do grafo que deseja procurar: ")
